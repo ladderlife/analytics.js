313c25aeb5082f1c9a3829f546f8ff02d1c739ac
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2016-08-10 12:26:32 -0700

    Fix integration.

--- analytics.js
+++ analytics.js
@@ -249,7 +249,7 @@ Zopim.prototype.load = function(done) {
 
   /* eslint-disable */
   window.$zopim||(function(d,s){var 
-z=$zopim=function(c){z._.push(c)},$=z.s=d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set._.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');$.src=scriptUrl;z.t=+new 
+z=window.$zopim=function(c){z._.push(c)},$=z.s=d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set._.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');$.src=scriptUrl;z.t=+new 
 Date;$.type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
   /* eslint-enable */
 
